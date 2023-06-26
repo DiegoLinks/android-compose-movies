319cf19ccc4574806036f9ddd89c72f8bcd4ee43
Diego de O. Silva <diego.rof@hotmail.com>
2023-06-26 15:50:24 -0300

    NetworkResult viewmodel status

--- app/src/main/java/com/compose/movies/presentation/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/compose/movies/presentation/feature/home/HomeViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.compose.movies.data.remote.NetworkResult
 import com.compose.movies.data.repository.MovieRepository
 import com.compose.movies.domain.model.Movie
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -19,10 +20,27 @@ class HomeViewModel @Inject constructor(
     private val _movies = MutableLiveData<List<Movie>>()
     val movies: LiveData<List<Movie>> = _movies
 
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> = _error
+
     fun getMovieList(apiKey: String) {
         viewModelScope.launch(Dispatchers.IO) {
-            val response = repository.getPopularMovies(apiKey)
-            _movies.postValue(response)
+            when (val response = repository.getPopularMovies(apiKey)) {
+                is NetworkResult.Success -> {
+                    _movies.postValue(response.data)
+                    _isLoading.postValue(false)
+                }
+                is NetworkResult.Error -> {
+                    _error.postValue(response.message)
+                    _isLoading.postValue(false)
+                }
+                is NetworkResult.Loading -> {
+                    _isLoading.value = true
+                }
+            }
         }
     }
 }
