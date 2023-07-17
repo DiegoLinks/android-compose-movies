f83fd43b625c8588cf3e41abc6b91705d243ad6a
Diego de O. Silva <diego.rof@hotmail.com>
2023-07-17 12:39:28 -0300

    Movie repository unit tests

--- app/src/main/java/com/compose/movies/domain/model/Country.kt
+++ app/src/main/java/com/compose/movies/domain/model/Country.kt
@@ -1,6 +1,16 @@
 package com.compose.movies.domain.model
 
-class Country(
-    val name: String,
-    val flagUrl: String,
-)
+import java.util.Objects
+
+class Country(val name: String, val flagUrl: String) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Country) return false
+
+        return name == other.name && flagUrl == other.flagUrl
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(name, flagUrl)
+    }
+}
